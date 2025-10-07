97c69b22e00aad9e2ee61e85002dddcd6dc95636
LiberSoft <info@libersoft.org>
2025-10-07 12:20:48 +0200

    Refactor sheep handling to +page.svelte from +layout.svelte.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import '../app.css';
-	import Sheep from '../components/Sheep.svelte';
-	import { getSettingsValue } from '../scripts/settings.ts';
-	let sheepEnabled = $state(getSettingsValue('sheepEnabled'));
-
-	onMount(() => {
-		// Listen for settings changes
-		const handleSettingsUpdate = (event: Event) => {
-			const customEvent = event as CustomEvent;
-			if (customEvent.detail.key === 'sheepEnabled') {
-				sheepEnabled = customEvent.detail.value;
-			}
-		};
-
-		window.addEventListener('settingsUpdate', handleSettingsUpdate);
-
-		return () => {
-			window.removeEventListener('settingsUpdate', handleSettingsUpdate);
-		};
-	});
 </script>
 
-{#if sheepEnabled}
-	<Sheep />
-{/if}
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { getSettingsValue } from '../scripts/settings.ts';
 	import Background from '../components/Background.svelte';
 	import Dashboard from '../components/Dashboard.svelte';
 	import Panel from '../components/Panel.svelte';
+	import Sheep from '../components/Sheep.svelte';
+
+	let sheepEnabled = $state(getSettingsValue('sheepEnabled'));
+
+	onMount(() => {
+		// Listen for settings changes
+		const handleSettingsUpdate = (event: Event) => {
+			const customEvent = event as CustomEvent;
+			if (customEvent.detail.key === 'sheepEnabled') {
+				sheepEnabled = customEvent.detail.value;
+			}
+		};
+
+		window.addEventListener('settingsUpdate', handleSettingsUpdate);
+
+		return () => {
+			window.removeEventListener('settingsUpdate', handleSettingsUpdate);
+		};
+	});
 </script>
 
 <svelte:head>
@@ -10,3 +31,6 @@
 <Background />
 <Dashboard />
 <Panel />
+{#if sheepEnabled}
+	<Sheep />
+{/if}
