3021f0519a846169765d440bc4f0d0e714f8acca
LiberSoft <info@libersoft.org>
2025-10-15 16:09:42 +0200

    Change server.js to server.ts in start.sh

---
+++ frontend/src/components/Input.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+	import Icon from '@/core/components/Icon/Icon.svelte';
+	interface Props {
+		type?: 'text' | 'number' | 'email' | 'password' | 'search' | 'tel' | 'url' | 'color';
+		placeholder?: string;
+		value?: string | number;
+		enabled?: boolean;
+		displayValue?: string | number;
+		expand?: boolean;
+		minWidth?: string;
+		maxWidth?: string;
+		onKeydown?: (e: KeyboardEvent) => void;
+		onFocus?: (e: FocusEvent) => void;
+		onBlur?: (e: FocusEvent) => void;
+		onClick?: (e: MouseEvent) => void;
+		min?: number;
+		max?: number;
+		step?: number;
+		icon?: {
+			img: string;
+			alt: string;
+			onClick?: (e: Event) => void;
+			colorVariable?: string;
+		};
+		inputRef?: HTMLInputElement;
+		'data-testid'?: string;
+		onChange?: (value: string | number) => void;
+	}
+	let { type = 'text', placeholder = '', value = $bindable(), enabled = true, displayValue, expand = true, minWidth, maxWidth, onKeydown, onFocus, onBlur, onClick, min, max, step, icon, inputRef = $bindable(), 'data-testid': testId, onChange }: Props = $props();
+
+	function handleKeydown(e) {
+		if (onKeydown) onKeydown(e);
+	}
+
+	function handleFocus(e) {
+		if (onFocus) onFocus(e);
+	}
+
+	function handleBlur(e) {
+		if (onBlur) onBlur(e);
+	}
+
+	function handleClick(e) {
+		if (onClick) onClick(e);
+	}
+
+	function handleChange(e: Event) {
+		const target = e.target as HTMLInputElement;
+		setValue(target.value);
+	}
+
+	function handleInput(e: Event) {
+		const target = e.target as HTMLInputElement;
+		setValue(target.value);
+	}
+
+	function setValue(newValue: string | number) {
+		value = type === 'number' ? Number(newValue) : newValue;
+		onChange?.(value);
+	}
+
+	export function focus() {
+		inputRef?.focus();
+	}
+</script>
+
+<style>
+	input {
+		box-sizing: border-box;
+		padding: 10px;
+		border-radius: 10px;
+		font-family: inherit;
+		font-size: inherit;
+		background-color: #fff;
+		color: #000;
+	}
+
+	.no-button {
+		border: 1px solid #000;
+	}
+
+	.button {
+		width: 100%;
+		border: 0;
+		outline: none;
+	}
+
+	input:focus {
+		outline: none;
+	}
+
+	input:disabled {
+		border: 1px solid #ccc;
+		background-color: #f5f5f5;
+		color: #ccc;
+	}
+
+	input.expand {
+		width: 100%;
+	}
+
+	.input-button-wrapper {
+		display: flex;
+		box-sizing: border-box;
+		align-items: center;
+		border: 1px solid #000;
+		border-radius: 10px;
+		background-color: #fff;
+	}
+
+	.input-button-wrapper:focus-within {
+		outline: 2px solid #000;
+	}
+</style>
+
+{#if icon}
+	<div class="input-button-wrapper" style:max-width={maxWidth && maxWidth} style:min-width={minWidth && minWidth}>
+		<input class="button" class:expand {type} {placeholder} bind:this={inputRef} bind:value onkeydown={e => handleKeydown(e)} oninput={handleInput} onfocus={handleFocus} onblur={handleBlur} onclick={handleClick} />
+		<Icon img={icon.img} alt={icon.alt} colorVariable={icon.colorVariable ? icon.colorVariable : ''} size="20px" padding="10px" onClick={icon.onClick} />
+	</div>
+{:else}
+	<input class="no-button" data-testid={testId} value={displayValue !== undefined ? displayValue : value} disabled={!enabled} onchange={handleChange} oninput={handleInput} class:expand style:max-width={maxWidth && maxWidth} style:min-width={minWidth && minWidth} {type} {placeholder} {min} {max} {step} bind:this={inputRef} onkeydown={e => handleKeydown(e)} onfocus={handleFocus} onblur={handleBlur} onclick={handleClick} />
+{/if}
